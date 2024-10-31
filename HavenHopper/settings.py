825eec2d237b2c67e9a843d1626464b94552d59b
Egwu1 <egwuchi03@gmail.com>
2024-10-31 17:39:20 +0100

    updated allowed hots

--- HavenHopper/settings.py
+++ HavenHopper/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["haven-a1c3.onrender.com", "127.0.0.1"]
 
 
 # Application definition
